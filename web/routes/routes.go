e2f466e096c5f13daad4e90674560dffab9c5db4
Yanke Guo <guoyk.cn@gmail.com>
2018-07-20 00:29:43 +0800

    using easy-autocomplete

--- web/routes/routes.go
+++ web/routes/routes.go
@@ -12,28 +12,10 @@ import (
 	"github.com/yankeguo/xlog/web/modules"
 )
 
-// Route mount all routes on nova.Nova
-func Route(n *nova.Nova) {
-	r := router.Route(n)
-	r.Get("/").Use(routeIndex)
-	r.Get("/:date").Use(routeShow)
-	r.Get("/:date/hints").Use(routeHints)
-}
-
-func routeIndex(c *nova.Context) (err error) {
-	now := time.Now()
-	http.Redirect(
-		c.Res,
-		c.Req,
-		fmt.Sprintf("/%04d-%02d-%02d", now.Year(), now.Month(), now.Day()),
-		http.StatusFound)
-	return
-}
-
-func dateInfo(date time.Time) string {
+func dateInfo(t time.Time) string {
 	now := time.Now()
 	today := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.UTC)
-	diff := date.Sub(today) / (time.Hour * 24)
+	diff := t.Sub(today) / (time.Hour * 24)
 	if diff == 0 {
 		return "今天"
 	} else if diff > 0 {
@@ -43,14 +25,37 @@ func dateInfo(date time.Time) string {
 	}
 }
 
+func extractURLDate(c *nova.Context) (date time.Time, dateStr string, err error) {
+	dateStr = router.PathParams(c).Get("date")
+	date, err = time.Parse("2006-01-02", dateStr)
+	return
+}
+
+func formatURLDate(t time.Time) string {
+	return fmt.Sprintf("%04d-%02d-%02d", t.Year(), t.Month(), t.Day())
+}
+
+// Route mount all routes on nova.Nova
+func Route(n *nova.Nova) {
+	r := router.Route(n)
+	r.Get("/").Use(routeIndex)
+	r.Get("/:date").Use(routeShow)
+	r.Get("/:date/hints").Use(routeHints)
+}
+
+func routeIndex(c *nova.Context) (err error) {
+	http.Redirect(c.Res, c.Req, "/"+formatURLDate(time.Now()), http.StatusFound)
+	return
+}
+
 func routeShow(c *nova.Context) (err error) {
 	// variables
 	v := view.Extract(c)
 	d := modules.Database(c)
 	// collection date
 	var date time.Time
-	dateStr := router.PathParams(c).Get("date")
-	if date, err = time.Parse("2006-01-02", dateStr); err != nil {
+	var dateStr string
+	if date, dateStr, err = extractURLDate(c); err != nil {
 		return
 	}
 	// collection
@@ -75,11 +80,6 @@ func routeShow(c *nova.Context) (err error) {
 	if err = coll.Execute(q, &results); err != nil {
 		return
 	}
-	// hints
-	var hints map[string]interface{}
-	if err = coll.Hints(&hints); err != nil {
-		return
-	}
 	// render
 	v.Data["Query"] = q
 	v.Data["Date"] = dateStr
@@ -87,7 +87,6 @@ func routeShow(c *nova.Context) (err error) {
 	v.Data["TotalCount"] = totalCount
 	v.Data["Stats"] = stats
 	v.Data["Results"] = results
-	v.Data["Hints"] = hints
 	v.HTML("show")
 	return
 }
@@ -98,8 +97,7 @@ func routeHints(c *nova.Context) (err error) {
 	d := modules.Database(c)
 	// collection date
 	var date time.Time
-	dateStr := router.PathParams(c).Get("date")
-	if date, err = time.Parse("2006-01-02", dateStr); err != nil {
+	if date, _, err = extractURLDate(c); err != nil {
 		return
 	}
 	// collection
